650c3a39ea5e1e3841e69794fac9043e1ecd177c
Meehai <mihaicristianpirvu@gmail.com>
2022-06-29 13:52:34 +0300

    added torchmetric_wrapper and updated the logic of metrics

--- lightning_module_enhanced/lightning_module_enhanced.py
+++ lightning_module_enhanced/lightning_module_enhanced.py
@@ -1,5 +1,6 @@
 """Generic Pytorch Lightning Graph module on top of a Graph module"""
 from typing import Dict, Callable, List, Union, Any, Sequence
+from copy import deepcopy
 from overrides import overrides
 import torch as tr
 from torch import optim, nn
@@ -9,10 +10,14 @@ from torchmetrics import Metric
 from nwutils.torch import tr_get_data, tr_to_device
 
 from .logger import logger
+from .torchmetric_wrapper import TorchMetricWrapper
 
 # pylint: disable=too-many-ancestors, arguments-differ, unused-argument, abstract-method
 class LightningModuleEnhanced(LightningModule):
-    """Pytorch Lightning module enhanced"""
+    """
+        Pytorch Lightning module enhanced. Callbacks and metrics are called based on the order described here:
+        https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#hooks
+    """
     def __init__(self, base_model: nn.Module, *args, **kwargs):
         assert isinstance(base_model, nn.Module)
         super().__init__()
@@ -21,8 +26,8 @@ class LightningModuleEnhanced(LightningModule):
         self.optimizer: optim.Optimizer = None
         self.scheduler_dict: optim.lr_scheduler._LRScheduler = None
         self.criterion_fn: Callable[[tr.Tensor, tr.Tensor], float] = None
-        self.metrics: Dict[str, Metric] = {}
-        self.logged_metrics: List[str] = []
+        self._metrics: Dict[str, Metric] = {}
+        self._logged_metrics: List[str] = []
         self._summary: ModelStatistics = None
         self.callbacks: List[Callback] = []
 
@@ -30,6 +35,7 @@ class LightningModuleEnhanced(LightningModule):
 
     @property
     def device(self) -> tr.device:
+        """Gets the device of the model, assuming all parameters are on the same device."""
         return next(self.base_model.parameters()).device
 
     @property
@@ -50,6 +56,7 @@ class LightningModuleEnhanced(LightningModule):
 
     @property
     def trainable_params(self) -> bool:
+        """Checks if the module is trainable"""
         return self.num_trainable_params > 0
 
     @trainable_params.setter
@@ -61,23 +68,66 @@ class LightningModuleEnhanced(LightningModule):
         # Reset summary such that it is recomputted if necessary (like for counting num trainable params)
         self._summary = None
 
+    @property
+    def metrics(self) -> List[str]:
+        return self._metrics
+
+    @metrics.setter
+    def metrics(self, metrics: Dict[str, Callable]):
+        if len(self._metrics) != 0:
+            logger.info(f"Overwriting existing metrics: {list(metrics.keys())}")
+        self._metrics = {}
+        for metric_name, metric_fn in metrics.items():
+            assert isinstance(metric_fn, (Metric, Callable)), \
+                f"Unknown metric type: '{type(metric_fn)}'. Expcted torchmetrics.Metric or Callable."
+            if not isinstance(metric_fn, Metric):
+                logger.debug(f"Metric '{metric_name}' is a callable. Converting to torchmetrics.Metric.")
+                metric_fn = TorchMetricWrapper(metric_fn)
+
+            self._metrics[metric_name] = metric_fn
+        self._metrics["loss"] = TorchMetricWrapper(self.criterion_fn)
+        self._metrics["loss"]._enable_grad = True
+        self.logged_metrics = list(self._metrics.keys())
+
+    @property
+    def logged_metrics(self) -> List[str]:
+        return self._logged_metrics
+
+    @logged_metrics.setter
+    def logged_metrics(self, logged_metrics: List[str]):
+        logger.info(f"Setting the logged metrics to {logged_metrics}")
+        diff = set(logged_metrics).difference(self._metrics.keys())
+        assert len(diff) == 0, f"Metrics {diff} are not in set metrics: {self._metrics.keys()}"
+        self._logged_metrics = logged_metrics
 
     # Pytorch lightning overrides
 
     @overrides
+    def on_train_start(self) -> None:
+        # We need to make a copy for all metrics if we have a validation dataloader, such that the global statistics
+        #  are unique for each of these datasets.
+        new_metrics = self.metrics
+        prefix = "val_"
+        if len(self.trainer.val_dataloaders) > 0:
+            for metric_name in self.logged_metrics:
+                new_metrics[f"{prefix}{metric_name}"] = deepcopy(self.metrics[metric_name])
+        self._metrics = new_metrics
+        return super().on_train_start()
+
+    @overrides
     def training_step(self, train_batch: Dict, batch_idx: int, *args, **kwargs) -> Union[tr.Tensor, Dict[str, Any]]:
         """Training step: returns batch training loss and metrics."""
-        return self._generic_step(train_batch)
+        return self._generic_batch_step(train_batch)
 
     @overrides
     def validation_step(self, train_batch: Dict, batch_idx: int, *args, **kwargs):
         """Training step: returns batch validation loss and metrics."""
-        return self._generic_step(train_batch, "val_")
+        return self._generic_batch_step(train_batch, prefix="val_")
 
     @overrides
     def test_step(self, train_batch: Dict, batch_idx: int, *args, **kwargs):
         """Training step: returns batch validation loss and metrics."""
-        return self._generic_step(train_batch, "test_")
+        return self._generic_batch_step(train_batch, prefix="test_")
 
     @overrides
     def configure_callbacks(self) -> Union[Sequence[Callback], Callback]:
@@ -86,17 +136,12 @@ class LightningModuleEnhanced(LightningModule):
     @overrides
     def training_epoch_end(self, outputs):
         """Computes epoch average train loss and metrics for logging."""
-        self._on_epoch_end(outputs)
-
-    @overrides
-    def validation_epoch_end(self, outputs):
-        """Computes epoch average validation loss and metrics for logging."""
-        self._on_epoch_end(outputs)
+        self._on_epoch_end()
 
     @overrides
     def test_epoch_end(self, outputs):
         """Computes average test loss and metrics for logging."""
-        self._on_epoch_end(outputs)
+        self._on_epoch_end()
 
     @overrides
     def configure_optimizers(self) -> Dict:
@@ -128,48 +173,43 @@ class LightningModuleEnhanced(LightningModule):
 
     # Internal methods
 
-    def _generic_train_step(self, train_batch: Dict):
-        """Generic step for computing the forward pass during training."""
-        x = train_batch["data"]
-        y = self.forward(x)
-        return y
-
-    def _generic_get_outputs(self, y, gt, prefix: str):
-        loss = self.criterion_fn(y, gt)
-        outputs = {f"{prefix}loss": loss}
-        for metric_name, metric_callback in self.metrics.items():
-            with tr.no_grad():
-                outputs[f"{prefix}{metric_name}"] = metric_callback(y, gt)
-        for metric_name in self.logged_metrics:
-            self.log(f"{prefix}{metric_name}", outputs[f"{prefix}{metric_name}"], prog_bar=True, on_step=True)
+    def _generic_batch_step(self, train_batch: Dict, prefix: str = "") -> Dict[str, tr.Tensor]:
+        """Generic step for computing the forward pass, loss and metrics."""
+        y = self.forward(train_batch["data"])
+        gt = tr_to_device(tr_get_data(train_batch["labels"]), self.device)
+        outputs = self._get_batch_metrics(y, gt, prefix)
         return outputs
 
-    def _generic_step(self, train_batch: Dict, prefix: str = ""):
-        """Generic step for computing the forward pass, loss and metrics."""
-        y = self._generic_train_step(train_batch)
-        gt = tr_to_device(tr_get_data(train_batch["labels"]), self.device)
-        return self._generic_get_outputs(y, gt, prefix)
+    def _get_batch_metrics(self, y, gt, prefix: str) -> Dict[str, tr.Tensor]:
+        """Get batch-level metrics"""
+        outputs = {}
+        for metric_name in self.logged_metrics:
+            prefixed_metric_name = f"{prefix}{metric_name}"
+            metric_fn: Metric = self.metrics[prefixed_metric_name]
+            metric_output = metric_fn.update(y, gt)
+            outputs[prefixed_metric_name] = metric_output
+            if metric_fn.compute_on_step:
+                self.log(prefixed_metric_name, outputs[prefixed_metric_name], prog_bar=True, on_step=True)
+        return outputs
 
     # pylint: disable=no-member
-    def _on_epoch_end(self, outputs):
-        keys = outputs[0].keys()
-        avgs = {}
-        for key in keys:
-            if key == "loss":
-                breakpoint()
-            items = tr.stack([x[key] for x in outputs])
-            # Shape: (N, ) or (N, 1) => just average it
-            if len(items.shape) == 1 or (len(items.shape) == 2 and items.shape[-1] == 1):
-                avgs[key] = items.mean()
-                self.log(key, avgs[key], prog_bar=True)
-            else:
-                avgs[key] = items.sum(dim=0)
-                # TODO: Log confusion matrix somehow using lightning
-                logger.info(f"{key} => {avgs[key].numpy()}")
+    def _on_epoch_end(self):
+        """Get epoch-level metrics"""
+        logged_metrics = self.logged_metrics
+        if len(self.trainer.val_dataloaders) > 0:
+            val_logged_metrics = [f"val_{metric_name}" for metric_name in logged_metrics]
+            logged_metrics = [*logged_metrics, *val_logged_metrics]
+        for metric_name in logged_metrics:
+            metric_fn = self.metrics[metric_name]
+            metric_epoch_result = metric_fn.compute()
+            self.log(metric_name, metric_epoch_result, on_epoch=True)
+            # Reset the metric after storing this epoch's value
+            metric_fn.reset()
 
     def reset_parameters(self):
         """Resets the parameters of the base model"""
         for layer in self.base_model.children():
+            assert hasattr(layer, "reset_parameters")
             layer.reset_parameters()
 
     def setup_module_for_train(self, train_cfg: Dict):
---
+++ lightning_module_enhanced/torchmetric_wrapper.py
@@ -0,0 +1,44 @@
+"""Wrapper than converts a Callable to a torchmetrics.Metric with compute_on_step=True that returns a number"""
+from typing import Callable, Dict
+from torchmetrics import Metric
+from overrides import overrides
+import torch as tr
+
+class TorchMetricWrapper(Metric):
+    """Wrapper for a regular callable torch metric"""
+    def __init__(self, metric_fn: Callable[[tr.Tensor, tr.Tensor], tr.Tensor]):
+        super().__init__(compute_on_step=True)
+        assert isinstance(metric_fn, Callable)
+        self.metric_fn = metric_fn
+        self.batch_results = tr.FloatTensor([0])
+        self.batch_count = tr.IntTensor([0])
+
+    @overrides(check_signature=False)
+    def update(self, preds: tr.Tensor, target: tr.Tensor) -> None:
+        """This is called at each batch end. It must be a number so .item() works."""
+        batch_res = self.metric_fn(preds, target)
+        batch_res_number = batch_res.item()
+        self.batch_results += batch_res_number
+        self.batch_count += 1
+        return batch_res
+
+    @overrides
+    def compute(self) -> tr.Tensor:
+        return self.batch_results / self.batch_count
+
+    @overrides
+    def reset(self):
+        super().reset()
+        self.batch_results *= 0
+        self.batch_count *= 0
+
+    def state_dict(self) -> Dict:
+        return {
+            "batch_results": self.batch_results,
+            "batch_count": self.batch_count
+        }
+
+    @overrides
+    def load_state_dict(self, state_dict: Dict, strict: bool = True):
+        self.batch_results = state_dict["batch_results"]
+        self.batch_count = state_dict["batch_count"]
