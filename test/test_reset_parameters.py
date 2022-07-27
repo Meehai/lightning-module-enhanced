a2bb0cebe2aa290d00e2449a31fc31f9cac44eb7
Meehai <mihaicristianpirvu@gmail.com>
2022-07-27 10:34:45 +0300

    added test for this fix as well

--- test/test_reset_parameters.py
+++ test/test_reset_parameters.py
@@ -4,20 +4,18 @@ from copy import deepcopy
 from lightning_module_enhanced import LightningModuleEnhanced
 from torch import nn
 
-# Test todo: parameter with no reset_parameters()
-# class SomeModule:
-#     def __init__(self):
+def test_reset_parameters_1():
+    module = LightningModuleEnhanced(nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1)))
+    params = deepcopy(tuple(module.parameters()))
+    module.reset_parameters()
+    new_params = deepcopy(tuple(module.parameters()))
+    for p1, p2 in zip(params, new_params):
+        assert not tr.allclose(p1, p2)
 
-class TestProperties:
-    @pytest.fixture(autouse=True)
-    def run_around_tests(self):
-        self.module = LightningModuleEnhanced(nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1)))
-        yield
-
-    def test_reset_parameters(self):
-        params = deepcopy(tuple(self.module.parameters()))
-        self.module.reset_parameters()
-        new_params = deepcopy(tuple(self.module.parameters()))
-        for p1, p2 in zip(params, new_params):
-            assert not tr.allclose(p1, p2)
-    
+def test_reset_parameters_2():
+    module = LightningModuleEnhanced(nn.Sequential(nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1))))
+    params = deepcopy(tuple(module.parameters()))
+    module.reset_parameters()
+    new_params = deepcopy(tuple(module.parameters()))
+    for p1, p2 in zip(params, new_params):
+        assert not tr.allclose(p1, p2)
