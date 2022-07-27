165102285663996222fc1b91b491e6b1ed8f1eeb
Meehai <mihaicristianpirvu@gmail.com>
2022-07-27 10:38:28 +0300

    deeper

--- test/test_reset_parameters.py
+++ test/test_reset_parameters.py
@@ -13,7 +13,25 @@ def test_reset_parameters_1():
         assert not tr.allclose(p1, p2)
 
 def test_reset_parameters_2():
+    """
+    This tests that if we have parameters(), but no reset_parameters(), then we'll try to recursively call
+    reset_parameters(), by first converting the model to a LME.
+    """
     module = LightningModuleEnhanced(nn.Sequential(nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1))))
+    params = deepcopy(tuple(module.parameters()))
+    module.reset_parameters()
+    new_params = deepcopy(tuple(module.parameters()))
+    for p1, p2 in zip(params, new_params):
+        assert not tr.allclose(p1, p2)
+
+def test_reset_parameters_3():
+    """
+    Same as above, but deeper. Fails for bigger values tho. Seems torch related somehow.
+    """
+    model = nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1))
+    for _ in range(10):
+        model = nn.Sequential(model)
+    module = LightningModuleEnhanced(model)
     params = deepcopy(tuple(module.parameters()))
     module.reset_parameters()
     new_params = deepcopy(tuple(module.parameters()))
