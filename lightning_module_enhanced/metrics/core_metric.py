further fixes to non numerical metrics and added test_and_fit test

lightning_module_enhanced/metrics/core_metric.py
@@ -67,8 +67,8 @@ class CoreMetric(nn.Module, ABC):
         any transformation (i.e. mlflow logger will sum a confusion matrix into a single number).
         By default, does nothing. Override this if needed.
         """
-        epoch_result_reduced = epoch_result
-        assert isinstance(epoch_result_reduced, tr.Tensor), f"Got {type(epoch_result_reduced)}"
+        assert isinstance(epoch_result, tr.Tensor), f"Got {type(epoch_result)}"
+        epoch_result_reduced = epoch_result.squeeze()
         shape = epoch_result_reduced.shape
         if not (len(shape) == 0 or (len(shape) == 1 and shape[-1] == 1)):
             logger.debug2(f"Metric '{self}' has a non-number reduced value (shape: {shape}). Returning None.")

test/core_module/test_fit_and_test.py
@@ -37,10 +37,10 @@ class Model(nn.Module):
 
 def test_fit_and_test_good():
     model = LightningModuleEnhanced(Model())
-    breakpoint()
     Trainer(max_epochs=1).fit(model, DataLoader(Reader()))
-
     res = Trainer().test(model, DataLoader(Reader()))
+    assert len(res) == 1
+    assert sorted(res[0].keys()) == ["loss", "metric1", "metric2"], res[0].keys()
 
 if __name__ == "__main__":
     test_fit_and_test_good()