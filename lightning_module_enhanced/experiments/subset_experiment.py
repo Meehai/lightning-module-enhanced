a6d15d4101117b53e33bebed83f7fdc7758215c2
Meehai <mihaicristianpirvu@gmail.com>
2022-10-24 18:11:49 +0300

    subset now supports explicit subsets, not just a linspace

--- lightning_module_enhanced/experiments/subset_experiment.py
+++ lightning_module_enhanced/experiments/subset_experiment.py
@@ -2,6 +2,7 @@
 Subset experiment module. Wrapper on top of a regular trainer to train the model n times with increasing sizes
 of the original dataset
 """
+from typing import Union, List
 from overrides import overrides
 from pytorch_lightning import Trainer
 from torch.utils.data import DataLoader, Subset
@@ -12,24 +13,27 @@ from .experiment import Experiment
 
 class SubsetExperiment(Experiment):
     """Subset experiment implementation"""
-    def __init__(self, trainer: Trainer, num_subsets: int):
+    def __init__(self, trainer: Trainer, subsets: Union[int, List[int]]):
         super().__init__(trainer)
-        self.num_subsets = num_subsets
+        assert isinstance(subsets, (int, list))
+        self.subsets = subsets
+        self.num_subsets = subsets if isinstance(subsets, int) else len(subsets)
         self.dataloaders = None
         self.tmp_train_dataloader = None
 
     def _do_plot(self):
-        ls = np.linspace(1 / self.num_subsets, 1, self.num_subsets)[0: len(self.df_fit_metrics)]
-        x = np.arange(len(self.df_fit_metrics))
+        ls = np.linspace(1 / self.subsets, 1, self.subsets) if isinstance(self.subsets, int) else self.subsets
+        x = ls[0: len(self.fit_metrics)]
+        xticks = [f"{_x*100:.2f}%" for _x in ls[0: len(x)]]
         metrics = self.df_fit_metrics.columns
         for metric in metrics:
             ys = self.df_fit_metrics[metric]
             plt.figure()
             plt.scatter(x, ys)
             plt.plot(x, ys)
-            plt.xticks(x, [f"{x*100:.2f}%" for x in ls])
             plt.xlabel("Percent used")
             plt.ylabel(f"Validation {metric}")
+            plt.xticks(x, xticks)
             plt.title(f"Subset experiment for {len(self._train_dataset)} train size")
             out_file = f"{self.trainer.logger.log_dir}/subset_val_{metric}.png"
             plt.savefig(out_file)
@@ -37,7 +41,7 @@ class SubsetExperiment(Experiment):
 
     @overrides
     def on_fit_start(self):
-        ls = np.linspace(1 / self.num_subsets, 1, self.num_subsets)
+        ls = np.linspace(1 / self.subsets, 1, self.subsets) if isinstance(self.subsets, int) else self.subsets
         subset_lens = [int(len(self._train_dataset) * x) for x in ls]
         indices = [np.random.choice(len(self._train_dataset), x, replace=False) for x in subset_lens]
         subsets = [Subset(self._train_dataset, ind) for ind in indices]
@@ -55,22 +59,6 @@ class SubsetExperiment(Experiment):
     @overrides
     def on_iteration_end(self, ix: int):
         return self._do_plot()
-
-    # @overrides
-    # def fit(self, model, train_dataloaders, val_dataloaders, *args, **kwargs):
-    #     """The main function, uses same args as a regular pl.Trainer"""
-    #     assert self.done is False, "Cannot fit twice"
-    #     self.fit_setup(model, train_dataloaders, val_dataloaders)
-
-    #     res = []
-    #     for i in range(self.num_subsets):
-    #         iter_res = self.do_one_iteration(i, self._model, self.dataloaders[i],
-    #                                          self._val_dataloaders, *args, **kwargs)
-    #         res.append(iter_res)
-    #         self._do_plot()
-    #     self.done = True
-    #     self.df_fit_metrics = pd.DataFrame(self.fit_metrics)
-    #     self.best_id = self.df_fit_metrics.iloc[self.df_fit_metrics["loss"].argmin()].index
 
     def __len__(self):
         return self.num_subsets
