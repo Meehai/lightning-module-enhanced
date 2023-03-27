f7eb362a00a551b1be21f146811cf27a5cdc7ceb
Meehai <mihaicristianpirvu@gmail.com>
2023-03-27 20:41:29 +0300

    support for all train setup except callbacks. callbacks are todo

--- lightning_module_enhanced/train_setup/train_setup.py
+++ lightning_module_enhanced/train_setup/train_setup.py
@@ -1,7 +1,11 @@
 """Module that implements a standard setup process of the lightning module via a train config file"""
 from typing import Dict
+from functools import partial
 from torch import optim
 from torch.nn import functional as F
+from torchmetrics.functional.classification import accuracy
+from torchmetrics.functional.regression.mae import mean_absolute_error
+from torchmetrics.functional.regression.mse import mean_squared_error
 
 from ..schedulers import ReduceLROnPlateauWithBurnIn
 from ..logger import logger
@@ -20,127 +24,84 @@ class TrainSetup:
         assert isinstance(train_cfg, Dict), f"Got {type(train_cfg)}"
         self.module = module
         self.train_cfg = train_cfg
+        self._setup()
 
-    def setup(self):
+    def _setup(self):
         """The main function of this class"""
-        if hasattr(self.module.base_model, "setup_model_for_train"):
-            logger.debug(f"Model {self.module.base_model} has setup_model_for_train() method. Calling it first.")
-            self.module.base_model.setup_model_for_train(self.train_cfg)
         if self.train_cfg is None:
-            logger.debug("Train cfg is None. Returning early.")
+            logger.info("Train cfg is None. Returning early.")
             return
+
         self._setup_optimizer()
-        self._setup_scheduler()
         self._setup_criterion()
+        self._setup_scheduler()
         self._setup_metrics()
         self._setup_callbacks()
 
-
-
     def _setup_optimizer(self):
+        assert "optimizer" in self.train_cfg, "Optimizer not in train cfg"
         cfg: Dict = self.train_cfg["optimizer"]
-        if self.module.optimizer is not None:
-            logger.debug2("Optimizer is already set. Returning early.")
-            return
 
-        if "optimizer" in self.train_cfg:
-            logger.debug2("Optimizer defined in train_cfg.")
-            optimizer_type = {
-                "adamw": optim.AdamW,
-                "adam": optim.Adam,
-                "sgd": optim.SGD,
-                "rmsprop": optim.RMSprop
-            }[cfg["type"]]
-            self.module.optimizer = optimizer_type(self.module.parameters(), **cfg["args"])
-            return
-
-        # Last hope is adding optimizer from base model
-        if not hasattr(self.module, "base_model"):
-            logger.warning(f"Provided module {self.module} has no 'base_model' property. Probably not a LME.")
-            return
-        
-        if hasattr(self.module.base_model, "optimizer") and self.module.base_model is not None:
-            logger.debug2("Optimizer set from base model")
-            self.module.optimizer = self.module.base_model.optimizer
-
-    def _setup_scheduler(self):
-        """Setup the scheduler following Pytorch Lightning's requirements."""
-        if self.module.scheduler_dict is not None:
-            logger.debug2("Scheduler was already set. Returning early")
-            return
-
-        if "scheduler" in self.train_cfg and self.train_cfg["scheduler"] is not None:
-            logger.debug2("Scheduler defined in train_cfg.")
-
-            assert self.module.optimizer is not None, "Cannot setup scheduler before optimizer."
-            scheduler_type = {
-                "ReduceLROnPlateau": optim.lr_scheduler.ReduceLROnPlateau,
-                "ReduceLROnPlateauWithBurnIn": ReduceLROnPlateauWithBurnIn,
-            }[self.train_cfg["scheduler"]["type"]]
-            scheduler = scheduler_type(optimizer=self.module.optimizer, **self.train_cfg["scheduler"]["args"])
-
-            if not "optimizer_args" in self.train_cfg["scheduler"]:
-                logger.debug2("Scheduler set, but no optimizer args. Adding a default to track 'val_loss'")
-                self.train_cfg["scheduler"]["optimizer_args"] = {"monitor": "val_loss"}
-
-            self.module.scheduler_dict = {"scheduler": scheduler, **self.train_cfg["scheduler"]["optimizer_args"]}
-            return
-
-        if not hasattr(self.module, "base_model"):
-            logger.warning(f"Provided module {self.module} has no 'base_model' property. Probably not a LME.")
-            return
-
-        # Last hope is adding scheduler from base model
-        if hasattr(self.module.base_model, "scheduler_dict") and self.module.base_model.scheduler_dict is not None:
-            logger.debug2("Scheduler set from base model")
-            try:
-                self.module.scheduler_dict = self.module.base_model.scheduler_dict
-            except Exception as e:
-                logger.warning(e)
+        logger.debug(f"Setting optimizer from config: '{cfg['type']}'")
+        optimizer_type = {
+            "adamw": optim.AdamW,
+            "adam": optim.Adam,
+            "sgd": optim.SGD,
+            "rmsprop": optim.RMSprop
+        }[cfg["type"]]
+        self.module.optimizer = optimizer_type(self.module.parameters(), **cfg["args"])
 
     def _setup_criterion(self):
         """Checks if the base model has the 'criterion_fn' property, and if True, uses this."""
-        if self.module.criterion_fn is not None:
-            logger.debug2("Criterion fn was already set. Returning early.")
+        assert "criterion" in self.train_cfg, "Train cfg has no criterion"
+        cfg = self.train_cfg["criterion"]
+
+        logger.debug(f"Setting the criterion to: '{cfg['type']}' based on provided cfg.")
+        criterion_type = {
+            "mse": F.mse_loss,
+            "l1": F.l1_loss,
+            "cross_entropy": F.cross_entropy
+        }[cfg["type"]]
+        self.module.criterion_fn = criterion_type
+
+    def _setup_scheduler(self):
+        """Setup the scheduler following Pytorch Lightning's requirements."""
+        if "scheduler" not in self.train_cfg:
+            logger.debug("Scheduler not defined in train_cfg. Returning early")
             return
 
-        if "criterion" in self.train_cfg:
-            cfg = self.train_cfg["criterion"]
-            assert cfg["type"] in ("mse", "cross_entropy"), f"Got an unexpected criterion type: '{cfg['type']}'"
-            logger.debug(f"Setting the criterion to: '{cfg['type']}' based on provided cfg.")
-            if cfg["type"] == "mse":
-                self.module.criterion_fn = F.mse_loss
-            if cfg["type"] == "cross_entropy":
-                self.module.criterion_fn = F.cross_entropy
-            return
-
-        if not hasattr(self.module, "base_model"):
-            logger.warning(f"Provided module {self.module} has no 'base_model' property. Probably not a LME.")
-            return
-
-        if hasattr(self.module.base_model, "criterion_fn") and self.module.base_model.criterion_fn is not None:
-            logger.debug("Base model has criterion_fn attribute. Using these by default")
-            self.module.criterion_fn = self.module.base_model.criterion_fn
+        cfg = self.train_cfg["scheduler"]
+        assert "type" in cfg and "optimizer_args" in cfg and "monitor" in cfg["optimizer_args"], cfg
+        assert self.module.optimizer is not None, "Cannot setup scheduler before optimizer."
+    
+        scheduler_type = {
+            "ReduceLROnPlateau": optim.lr_scheduler.ReduceLROnPlateau,
+            "ReduceLROnPlateauWithBurnIn": ReduceLROnPlateauWithBurnIn,
+        }[cfg["type"]]
+        scheduler = scheduler_type(optimizer=self.module.optimizer, **cfg["args"])
+        self.module.scheduler_dict = {"scheduler": scheduler, **cfg["optimizer_args"]}
 
     def _setup_metrics(self):
-        """Checks if the base model has the 'metrics' property, and if True, uses it."""
-        if len(self.module.metrics) > 1:
-            logger.debug2("Metrics were already set. Returning early.")
+        """Setup the metrics from the config file. Only a couple of them are available."""
+        if "metrics" not in self.train_cfg:
+            logger.debug("Metrics not defined in train_cfg. Returning early")
             return
 
-        if hasattr(self.module.base_model, "metrics") and self.module.base_model.metrics is not None:
-            logger.debug("Base model has metrics attribute. Using these by default")
-            self.module.metrics = self.module.base_model.metrics
+        metrics = {}
+        cfg = self.train_cfg["metrics"]
+        for metric_dict in cfg:
+            metric_type, metric_args = metric_dict["type"], metric_dict.get("args", {})
+            assert metric_type in ("accuracy", "l1", "mse"), metric_type
+            if metric_type == "accuracy":
+                assert "num_classes" in metric_args and "task" in metric_args
+                metrics[metric_type] = (partial(accuracy, **metric_args), "max")
+            if metric_type == "l1":
+                assert metric_args == {}
+                metrics[metric_type] = (mean_absolute_error, "min")
+            if metric_type == "mse":
+                assert metric_args == {}
+                metrics[metric_type] = (mean_squared_error, "min")
+        self.module.metrics = metrics
 
     def _setup_callbacks(self):
-        """Checks if the base model has the 'callbacks' property, and if True, uses it."""
-        if len(self.module.callbacks) > len(self.module._default_callbacks):  # pylint: disable=protected-access
-            logger.debug2("Callbacks were already set. Returning early.")
-            return
-
-        if hasattr(self.module.base_model, "callbacks") and self.module.base_model.callbacks is not None:
-            logger.debug("Base model has callbacks attribute. Using these by default")
-            self.module.callbacks = self.module.base_model.callbacks
-
-    def __call__(self):
-        return self.setup()
+        """TODO: callbacks"""
--- test/unit/train_setup/test_train_setup.py
+++ test/unit/train_setup/test_train_setup.py
@@ -1,10 +1,21 @@
+from lightning_module_enhanced import LME, TrainSetup
+from lightning_lite.utilities.exceptions import MisconfigurationException
 from pytorch_lightning import Trainer
 from torch.utils.data import DataLoader
 from torch import nn, optim
 import torch as tr
 
-from lightning_module_enhanced import LME, TrainSetup
+class ReaderClassification:
+    def __init__(self, n_dims: int = 1):
+        self.n_dims = n_dims
 
+    def __len__(self):
+        return 10
+
+    def __getitem__(self, ix):
+        res = tr.zeros(self.n_dims)
+        res[tr.randn(self.n_dims).argmax()] = 1
+        return {"data": tr.randn(2), "labels": res}
 
 class Reader:
     def __len__(self):
@@ -13,44 +24,93 @@ class Reader:
     def __getitem__(self, ix):
         return {"data": tr.randn(2), "labels": tr.randn(1)}
 
+def test_all_are_none():
+    model = LME(nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1)))
+    assert len(model.metrics) == 0
+    assert model.criterion_fn is None
+    assert model.optimizer is None
+    assert model.scheduler_dict is None
+    try:
+        Trainer(max_epochs=1).fit(model, DataLoader(Reader()))
+        raise Exception
+    except ValueError:
+        pass
 
-class BaseModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.sequential = nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1))
 
-    def forward(self, x):
-        return self.sequential(x)
-
-class BaseModelPlusSetup(BaseModel):
-    @property
-    def criterion_fn(self):
-        return lambda y, gt: (y - gt).pow(2).mean()
-
-    @property
-    def optimizer(self):
-        return optim.SGD(self.parameters(), lr=0.01)
-
-    
-def test_fit_good_implicit():
-    model = LME(BaseModelPlusSetup())
-    TrainSetup(model, {})
+def test_train_setup_minimal():
+    model = LME(nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1)))
+    cfg = {
+        "optimizer": {"type": "sgd", "args": {"lr": 0.01}},
+        "criterion": {"type": "mse"}
+    }
+    TrainSetup(model, cfg)
     Trainer(max_epochs=1).fit(model, DataLoader(Reader()))
 
+def test_train_setup_scheduler_bad():
+    model = LME(nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1)))
+    cfg = {
+        "optimizer": {"type": "sgd", "args": {"lr": 0.01}},
+        "criterion": {"type": "mse"},
+        "scheduler": {"type": "ReduceLROnPlateau", "args": {"factor": 0.9, "patience": 5}}
+    }
+    try:
+        TrainSetup(model, cfg)
+        raise Exception
+    except AssertionError:
+        pass
 
-def test_fit_explicit_over_implicit_1():
-    model = LME(BaseModelPlusSetup())
+def test_train_setup_scheduler_bad_2():
+    model = LME(nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1)))
+    cfg = {
+        "optimizer": {"type": "sgd", "args": {"lr": 0.01}},
+        "criterion": {"type": "mse"},
+        "scheduler": {"type": "ReduceLROnPlateau",
+                      "args": {"factor": 0.9, "patience": 5},
+                      "optimizer_args": {"monitor": "val_loss"}}
+    }
+    TrainSetup(model, cfg)
+    """fails because no val_loss is available"""
+    try:
+        Trainer(max_epochs=1).fit(model, DataLoader(Reader()))
+        raise Exception
+    except MisconfigurationException:
+        pass
 
-    def f(y, gt):
-        return (y - gt).abs().mean()
+def test_train_setup_scheduler_good():
+    model = LME(nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1)))
+    cfg = {
+        "optimizer": {"type": "sgd", "args": {"lr": 0.01}},
+        "criterion": {"type": "mse"},
+        "scheduler": {"type": "ReduceLROnPlateau",
+                      "args": {"factor": 0.9, "patience": 5},
+                      "optimizer_args": {"monitor": "loss"}}
+    }
+    TrainSetup(model, cfg)
+    Trainer(max_epochs=1).fit(model, DataLoader(Reader()))    
 
-    model.criterion_fn = f
-    Trainer(max_epochs=1).fit(model, DataLoader(Reader()))
-    assert model.criterion_fn.metric_fn == f
+def test_train_setup_metrics_bad_accuracy():
+    model = LME(nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1)))
+    cfg = {
+        "optimizer": {"type": "sgd", "args": {"lr": 0.01}},
+        "criterion": {"type": "mse"},
+        "metrics": [{"type": "accuracy"}]
+    }
+    try:
+        TrainSetup(model, cfg)
+        raise Exception
+    except AssertionError:
+        pass
 
 
-def test_fit_explicit_over_implicit_2():
-    model = LME(BaseModelPlusSetup())
-    model.optimizer = optim.Adam(model.parameters(), lr=0.01)
-    Trainer(max_epochs=1).fit(model, DataLoader(Reader()))
-    assert isinstance(model.optimizer, optim.Adam)
+def test_train_setup_metrics_good_accuracy():
+    model = LME(nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 5), nn.ReLU()))
+    cfg = {
+        "optimizer": {"type": "sgd", "args": {"lr": 0.01}},
+        "criterion": {"type": "mse"},
+        "metrics": [{"type": "accuracy", "args": {"task": "multiclass", "num_classes": 5, "average": "none"}}]
+    }
+    TrainSetup(model, cfg)
+    assert model.metadata_callback.metadata is None
+    Trainer(max_epochs=7).fit(model, DataLoader(ReaderClassification(n_dims=5)))
+    assert len(model.metadata_callback.metadata["epoch_metrics"]["accuracy"]) == 7
+    assert len(model.metadata_callback.metadata["epoch_metrics"]["accuracy"][0]) == 5
