db50fda61bf21e1d3ba40080c35a59aaa572ca29
Mihai Cristian Pirvu <mihaicristianpirvu@gmail.com>
2023-05-18 09:17:41 +0300

    fixed a bug in plotmetrics I think

--- lightning_module_enhanced/callbacks/plot_metrics.py
+++ lightning_module_enhanced/callbacks/plot_metrics.py
@@ -57,13 +57,15 @@ class PlotMetrics(Callback):
             if metric_name not in self.history:
                 logger.warning(f"Metric '{metric_name}' not in original metrics, probably added afterwards. Skipping")
                 continue
-            metric_score = pl_module._active_run_metrics[""][metric_name].epoch_result_reduced(None)
+            metric = pl_module._active_run_metrics[""][metric_name]
+            metric_score = metric.epoch_result_reduced(metric.epoch_result())
             if metric_score is None:
                 logger.debug2(f"Metric '{metric_name}' cannot be reduced to a single number. Skipping")
                 continue
             self.history[metric_name]["train"].append(metric_score.item())
             if trainer.enable_validation:
-                val_metric_score = pl_module._active_run_metrics["val_"][metric_name].epoch_result_reduced(None)
+                val_metric = pl_module._active_run_metrics["val_"][metric_name]
+                val_metric_score = val_metric.epoch_result_reduced(val_metric.epoch_result())
                 self.history[metric_name]["val"].append(val_metric_score.item())
 
             out_file = f"{trainer.loggers[0].log_dir}/{metric_name}.png"
