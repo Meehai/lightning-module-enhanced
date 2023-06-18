517dd91eaadb14458505555d5e00f579e64a1137
Meehai <mihaicristianpirvu@gmail.com>
2023-06-18 16:25:20 +0300

    pylint

--- lightning_module_enhanced/__init__.py
+++ lightning_module_enhanced/__init__.py
@@ -1,10 +1,10 @@
 """Init module"""
+import logging
 
 # pylint: disable=reimported
 from .core_module import CoreModule as LME, CoreModule as LightningModuleEnhanced
 from .trainable_module import TrainableModule
 from .train_setup import TrainSetup
 
-import logging
 # disable seed messages from pytorch lightning
 logging.getLogger("lightning_fabric.utilities.seed").setLevel(logging.CRITICAL)
