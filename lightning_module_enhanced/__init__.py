d9da3a23eaefb562e6c4d3bbbc21b1d4b5afb017
Meehai <mihaicristianpirvu@gmail.com>
2023-06-19 14:07:59 +0300

    just shut up please

--- lightning_module_enhanced/__init__.py
+++ lightning_module_enhanced/__init__.py
@@ -1,5 +1,6 @@
 """Init module"""
 import logging
+import warnings
 
 # pylint: disable=reimported
 from .core_module import CoreModule as LME, CoreModule as LightningModuleEnhanced
@@ -8,3 +9,6 @@ from .train_setup import TrainSetup
 
 # disable seed messages from pytorch lightning
 logging.getLogger("lightning_fabric.utilities.seed").setLevel(logging.CRITICAL)
+logging.getLogger("pytorch_lightning.utilities.rank_zero").setLevel(logging.CRITICAL)
+warnings.filterwarnings("ignore", category=UserWarning, module="pytorch_lightning")
+warnings.filterwarnings("ignore", category=UserWarning, module="lightning_fabric")
