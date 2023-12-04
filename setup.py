9cf02897cd7d532b98f158b06d08134f56e78c84
Meehai <mihaicristianpirvu@gmail.com>
2023-12-04 12:38:36 +0200

    cache_results

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_reqs(requirements):
     return required, dependency_links
 
 name = "lightning-module-enhanced"
-version = "0.1"
+version = "0.2"
 description = "Lightning Module Enhanced"
 url = "https://gitlab.com/mihaicristianpirvu/lightning-module-enhanced"
 
