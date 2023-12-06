36f564b294e0d258f9494f9ab10f7134fd9ccf78
Meehai <mihaicristianpirvu@gmail.com>
2023-12-06 10:07:55 +0200

    updates to Plotcallback too

--- lightning_module_enhanced/callbacks/plot_callback.py
+++ lightning_module_enhanced/callbacks/plot_callback.py
@@ -4,31 +4,28 @@ from pathlib import Path
 from overrides import overrides
 from pytorch_lightning import Trainer, LightningModule
 from pytorch_lightning.callbacks import Callback
-import torch as tr
 
 from ..logger import logger
 
 class PlotCallbackGeneric(Callback):
     """Plot callback impementation. For each train/validation epoch, create a dir under logger_dir/pngs/epoch_X"""
-    def __init__(self, plot_callback: Callable):
+    def __init__(self, plot_callback: Callable, mkdir: bool = True):
         self.plot_callback = plot_callback
+        self.mkdir = mkdir
 
     @staticmethod
-    def _get_out_dir(trainer: Trainer, dir_name: str) -> Path:
+    def _get_out_dir(trainer: Trainer, dir_name: str, mkdir: bool) -> Path | None:
         """Gets the output directory as '/path/to/log_dir/pngs/train_or_val/epoch_N/' """
         if len(trainer.loggers) == 0:
             return None
         out_dir = Path(f"{trainer.loggers[0].log_dir}/pngs/{dir_name}/{trainer.current_epoch + 1}")
-        out_dir.mkdir(exist_ok=True, parents=True)
+        if mkdir:
+            out_dir.mkdir(exist_ok=True, parents=True)
         return out_dir
 
     def _get_prediction(self, pl_module: LightningModule, batch):
-        if hasattr(pl_module, "cache_result") and pl_module.cache_result is not None:
-            y = pl_module.cache_result
-        else:
-            logger.debug2("Prediction was not cached or PlotCallback. Recomputing it. This may be expensive!")
-            with tr.no_grad():
-                y = pl_module.forward(batch)
+        assert hasattr(pl_module, "cache_result") and pl_module.cache_result is not None
+        y = pl_module.cache_result
         return y
 
     def _do_call(self, trainer: Trainer, pl_module: LightningModule, batch, batch_idx, key, prediction):
@@ -37,7 +34,7 @@ class PlotCallbackGeneric(Callback):
         if len(trainer.loggers) == 0:
             logger.warning("No lightning logger found. Not calling PlotCallbacks()")
             return
-        out_dir = PlotCallbackGeneric._get_out_dir(trainer, key)
+        out_dir = PlotCallbackGeneric._get_out_dir(trainer, key, self.mkdir)
         self.plot_callback(model=pl_module, batch=batch, y=prediction, out_dir=out_dir)
 
     @overrides
@@ -67,6 +64,6 @@ class PlotCallback(PlotCallbackGeneric):
             logger.warning("No lightning logger found. Not calling PlotCallbacks()")
             return
 
-        out_dir = PlotCallbackGeneric._get_out_dir(trainer, key)
+        out_dir = PlotCallbackGeneric._get_out_dir(trainer, key, self.mkdir)
         x, gt = batch["data"], batch["labels"]
         self.plot_callback(x=x, y=prediction, gt=gt, out_dir=out_dir, model=pl_module)
