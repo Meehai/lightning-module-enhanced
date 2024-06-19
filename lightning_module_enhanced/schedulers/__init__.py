201763414a7ad0ec3581c1aaae5a316e1d8705fd
Meehai <mihaicristianpirvu@gmail.com>
2024-06-19 12:53:34 +0300

    schedulers in our code

--- lightning_module_enhanced/schedulers/__init__.py
+++ lightning_module_enhanced/schedulers/__init__.py
@@ -1,2 +1,4 @@
 """Init file"""
 from .reduce_lr_on_plateau_with_burn_in import ReduceLROnPlateauWithBurnIn
+from .warmup_cosine_scheduler import WarmupCosineSchedule
+from torch.optim.lr_scheduler import *
