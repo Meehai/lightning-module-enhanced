f1825f872271cc2eb6382770523da6d8a3289c4f
Meehai <mihaicristianpirvu@gmail.com>
2024-06-19 12:21:29 +0300

    metrics is actually needed here hmm

--- lightning_module_enhanced/lme.py
+++ lightning_module_enhanced/lme.py
@@ -400,6 +400,7 @@ class LightningModuleEnhanced(TrainableModuleMixin, pl.LightningModule):
 
         is_val = (monitor := self.scheduler["monitor"]).startswith("val_")
         monitor = monitor[4:] if is_val else monitor
+        prefix = "val_" if is_val else ""
         if is_val and "val_" not in self._active_run_metrics:
             raise MisconfigurationException(f"Monitor: {monitor} but no validation set provided")
         if monitor not in self.metrics:
@@ -407,4 +408,5 @@ class LightningModuleEnhanced(TrainableModuleMixin, pl.LightningModule):
         if self.trainer.current_epoch == 0:
             return
 
-        self.scheduler["scheduler"].step()
+        metric = self._active_run_metrics[prefix][monitor] # pylint: disable=protected-access
+        self.scheduler["scheduler"].step(metric.epoch_result_reduced(metric.epoch_result()))
