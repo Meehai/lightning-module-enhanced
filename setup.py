2f693ce225a3e7e6106cc6de3143769817c061e1
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2024-09-17 16:29:06 +0000

    nan to num in PlotMetrics

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os import path
 
 name = "lightning-module-enhanced"
-version = "0.32.9"
+version = "0.32.11"
 description = "Lightning Module Enhanced"
 url = "https://gitlab.com/mihaicristianpirvu/lightning-module-enhanced"
 
