c9aec36c132960c2c18b990d335e629bf8ae98df
meehai <mihaicristianpirvu@gmail.com>
2024-09-20 12:00:23 +0300

    remove CopyBestCheckpoint

--- lightning_module_enhanced/callbacks/__init__.py
+++ lightning_module_enhanced/callbacks/__init__.py
@@ -2,5 +2,4 @@
 from .plot_callback import PlotCallback, PlotCallbackGeneric
 from .plot_metrics import PlotMetrics
 from .metadata_callback import MetadataCallback
-from .copy_best_checkpoint import CopyBestCheckpoint
 from .remove_previous_train_artifacts import RemovePreviousTrainArtifacts
