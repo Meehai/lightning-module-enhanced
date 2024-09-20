9cb1fd5c7a18f9f35b9a77ed18b7675644df44b2
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2024-09-20 07:07:32 +0000

    Release: 0.33.0 + deps versions upgrade

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os import path
 
 name = "lightning-module-enhanced"
-version = "0.32.12"
+version = "0.33.0"
 description = "Lightning Module Enhanced"
 url = "https://gitlab.com/mihaicristianpirvu/lightning-module-enhanced"
 
@@ -10,8 +10,8 @@ loc = path.abspath(path.dirname(__file__))
 with open(f"{loc}/README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-required = ["torch>=2.2.1,<3.0.0", "pytorch_lightning==2.4.0", "torchinfo==1.8.0", "torchmetrics==1.4.1",
-            "overrides==7.7.0", "matplotlib==3.9.0", "loggez>=0.3", "tensorboardX==2.6.2.2", "pool-resources==0.3.0"]
+required = ["torch==2.4.1", "pytorch_lightning==2.4.0", "torchinfo==1.8.0", "torchmetrics==1.4.1",
+            "overrides==7.7.0", "matplotlib==3.9.0", "loggez==0.3", "pool-resources==0.3.0"]
 
 setup(
     name=name,
